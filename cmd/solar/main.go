f1e8865c9b29bd9b39933111fce861ac6bbf4f1b
diamondburned <diamond@arikawa-hi.me>
2022-12-11 22:49:23 -0800

    Fix up cmd/solar/

--- cmd/solar/main.go
+++ cmd/solar/main.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 
 	"github.com/diamondburned/solar"
@@ -17,8 +19,10 @@ var (
 	longitude = solar.LocalLongitude()
 	lowTemp   = float64(solar.DefaultLowTemperature)
 	highTemp  = float64(solar.DefaultHighTemperature)
-	address   = ""
-	tformat   = time.RFC3339Nano
+	tformat   = "15:04:05"
+	tnow      = time.Now().Unix()
+	useIPLoc  = false
+	printJSON = false
 )
 
 func main() {
@@ -26,57 +30,110 @@ func main() {
 	flag.Float64Var(&longitude, "long", longitude, "longitude, optional")
 	flag.Float64Var(&lowTemp, "lo", lowTemp, "lowest temperature in Kelvin")
 	flag.Float64Var(&highTemp, "hi", highTemp, "highest temperature in Kelvin")
-	flag.StringVar(&address, "a", address, "address to geoguess using geocode.xyz, uses internet")
 	flag.StringVar(&tformat, "t", tformat, "time format")
+	flag.Int64Var(&tnow, "now", tnow, "current time in Unix seconds")
+	flag.BoolVar(&printJSON, "j", printJSON, "print JSON instead of human-readable")
+	flag.BoolVar(&useIPLoc, "ip", useIPLoc, "use IP location instead of coordinates")
 	flag.Parse()
 
-	if address != "" {
-		r, err := geocode(address)
+	var geocodeResponse *geocodeResponse
+	if useIPLoc {
+		ip, err := myIP()
 		if err != nil {
-			log.Fatalln("cannot resolve address:", err)
+			log.Fatal("cannot get public IP address: ", err)
 		}
-		printlnf("location: %s, %s", r.Standard.City, r.Standard.Province)
-		latitude = r.Latitude
-		longitude = r.Longitude
+
+		geocodeResponse, err = geocode(ip)
+		if err != nil {
+			log.Fatalln("cannot geolocate from public IP:", err)
+		}
+
+		latitude = geocodeResponse.Latitude
+		longitude = geocodeResponse.Longitude
 	}
-
-	printlnf("latitude: %g", latitude)
-	printlnf("longitude: %g", longitude)
-
-	now := time.Now()
-	sun := solar.CalculateSun(now, latitude, longitude)
-
-	printlnf("sun condition: %s", sun.Condition)
-	printTime("dawn time", sun.Dawn)
-	printTime("sunrise time", sun.Sunrise)
-	printTime("sunset time", sun.Sunset)
-	printTime("dusk time", sun.Dusk)
 
 	lo := solar.Temperature(lowTemp)
 	hi := solar.Temperature(highTemp)
+	temp, sun := solar.CalculateTemperature(time.Unix(tnow, 0), latitude, longitude, lo, hi)
 
-	printlnf("current color temperature: %.0fK",
-		solar.CalculateTemperature(now, latitude, longitude, lo, hi))
-}
+	r := results{
+		Latitude:    latitude,
+		Longitude:   longitude,
+		Geocode:     geocodeResponse,
+		Temperature: temp,
+		Sun:         sun,
+	}
 
-func printTime(name string, t time.Time) {
-	if t.IsZero() {
-		printlnf("%s: null", name)
+	if printJSON {
+		r.PrintJSON(os.Stdout)
 	} else {
-		printlnf("%s: %s", name, t.Format(tformat))
+		r.PrintText(os.Stdout)
 	}
 }
 
-func printlnf(f string, v ...interface{}) {
-	fmt.Printf(f+"\n", v...)
+type results struct {
+	Latitude    float64           `json:"latitude"`
+	Longitude   float64           `json:"longitude"`
+	Geocode     *geocodeResponse  `json:"geocode,omitempty"`
+	Temperature solar.Temperature `json:"temperature"`
+	Sun         solar.Sun         `json:"sun"`
+}
+
+func (r results) PrintText(w io.Writer) {
+	printlnf := func(f string, v ...interface{}) {
+		fmt.Fprintln(w, fmt.Sprintf(f, v...))
+	}
+	printTime := func(name string, t time.Time) {
+		if !t.IsZero() {
+			fmt.Fprintf(w, "%s: %s\n", name, t.Format(tformat))
+		}
+	}
+
+	printlnf("latitude: %g", r.Latitude)
+	printlnf("longitude: %g", r.Longitude)
+	if r.Geocode != nil {
+		printlnf("location: %s, %s", r.Geocode.City, r.Geocode.Region)
+	}
+	printlnf("sun condition: %s", r.Sun.Condition)
+	printTime("dawn time", r.Sun.Dawn)
+	printTime("sunrise time", r.Sun.Sunrise)
+	printTime("sunset time", r.Sun.Sunset)
+	printTime("dusk time", r.Sun.Dusk)
+	printlnf("color temperature: %.0fK", r.Temperature)
+}
+
+func (r results) PrintJSON(w io.Writer) {
+	e := json.NewEncoder(w)
+	e.SetIndent("", "  ")
+	if err := e.Encode(r); err != nil {
+		log.Panicln("cannot encode JSON:", err)
+	}
+}
+
+func myIP() (string, error) {
+	r, err := http.Get("https://ifconfig.me")
+	if err != nil {
+		return "", err
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("status code %d", r.StatusCode)
+	}
+
+	v, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(v), nil
 }
 
 type geocodeResponse struct {
-	Standard struct {
-		City     string `json:"city"`
-		Province string `json:"prov"`
-		Country  string `json:"countryname"`
-	} `json:"standard"`
+	City      string  `json:"city"`
+	Province  string  `json:"prov"`
+	Region    string  `json:"region"`
+	Country   string  `json:"country"`
 	Longitude float64 `json:"longt,string"`
 	Latitude  float64 `json:"latt,string"`
 }
